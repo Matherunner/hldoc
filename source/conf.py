4ffbdaa5a2bcfd1327b3d6e5173ec8c022ea3c87
Jiangwei Chong <jw@jwchong.com>
2025-12-30 12:21:33 +0800

    Add proof

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@ author = "Jiangwei Chong"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_proof"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "venv"]
@@ -40,27 +40,10 @@ html_title = "Half-Life Physics Reference"
 
 html_theme_options = {}
 
-# html_theme_options = {
-# "logo": "hlprlogo.png",
-# "description": "An attempt to document and exploit the in-game physics of Half-Life.",
-# "font_size": "18px",
-# "font_family": "sans-serif",
-# "page_width": "1150px",
-# "sidebar_width": "250px",
-# "code_font_size": "85%",
-# "base_bg": "#fafaf4",
-# "body_text": "rgb(59, 50, 40)",
-# "analytics_id": "UA-67469786-1",
-# }
-
-# html_sidebars = {
-#     "**": ["sidebar-nav-bs", "sidebar-ethical-ads"],
-# }
-
 html_last_updated_fmt = "%b %d, %Y"
 
 html_static_path = ["static"]
-html_css_files = ["css/custom.css"]
+html_css_files = []
 
 html_copy_source = False
 html_show_sourcelink = False
